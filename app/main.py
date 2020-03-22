30f71794d90e3ec382eeaaff99a97e4ee35e865a
Aman <asdevofficial@gmail.com>
2020-03-22 21:21:56 +0530

    Added vector file creating algorithm

--- app/main.py
+++ app/main.py
@@ -53,7 +53,7 @@ def check_env():
         print("Git missing")
         missing.append("git")
     try:
-        os.system("opencv_createsamples --help")
+        os.system("opencv_createsamples")
     except:
         print("Create Samples missing")
         missing.append("libopencv")
@@ -118,7 +118,7 @@ def generate_positive_list():
         print("Unable to open positive images idnex file ||| STOP PROGRESS")
     images = index.readlines()
     try:
-        pos_list = open(output_entry_variable.get()+"/positive.lst", 'a')
+        pos_list = open("positive.lst", 'a')
     except:
         print("Unable to generate positive.lst file ||| STOP PROGRESS")
     for i in range (0, len(images)):
@@ -126,12 +126,27 @@ def generate_positive_list():
         pos_list.write(images[i].split('\n')[0]+" 1 0 0 "+str(width)+" "+str(height)+"\n")
     pos_list.close()
     index.close()
+    generate_positive_vector()
+
+def generate_positive_vector():
+    try:
+        pos_list = open("positive.lst")
+    except:
+        print("Unable to read the file at: ",output_entry_variable.get()+"/positive.lst"," ||| STOP PROCRESS")
+    file_len = len(pos_list.readlines())
+    usage_num = '1'
+    try:
+        os.system("opencv_createsamples -info positive.lst -num "+usage_num+" -w "+image_width_variable.get()+" -h "+image_height_variable.get()+" -vec "+output_entry_variable.get()+"/positive.vec")
+    except:
+        print("Unable to create positive.vec file")
+    os.remove('positive.lst')
 
         
 def start_training():
     print("training")
     print("Positive image location: ",positive_entry_variable.get())
     print("Negative image location: ",negative_entry_variable.get())
+    os.system('cd '+output_entry_variable.get())
     generate_index()
 
 #Starting the application and checking all required libraries
@@ -148,21 +163,36 @@ main_window.title("HAAR CASCADE CLASSIFIER")
 
 head_status = tk.Label(main_window, text="Starting", bg="#33ff8a", pady=3).grid(column=0,row=0, columnspan=1)
 
-positive_lable = tk.Label(main_window,text="Positive image dataset location", pady=3).grid(column=0,row=1)
+positive_label = tk.Label(main_window,text="Positive image dataset location", pady=3).grid(column=0,row=1)
 positive_entry_variable = tk.StringVar()
 positive_entry = tk.Entry(main_window, textvariable=positive_entry_variable).grid(column=1, row=1)
 positive_entry_btn = tk.Button(text="...", height = 2, width=10, command = open_pos_dir_chooser).grid(column=2, row=1)
 
-negative_lable = tk.Label(main_window,text="Negative image dataset location", pady=3).grid(column=0,row=2)
+negative_label = tk.Label(main_window,text="Negative image dataset location", pady=3).grid(column=0,row=2)
 negative_entry_variable = tk.StringVar()
 negative_entry = tk.Entry(main_window, textvariable=negative_entry_variable).grid(column=1, row=2)
 negative_entry_btn = tk.Button(text="...", height = 2, width=10, command = open_neg_dir_chooser).grid(column=2, row=2)
 
-output_lable = tk.Label(main_window,text="Output location", pady=3).grid(column=0,row=3)
+output_label = tk.Label(main_window,text="Output location", pady=3).grid(column=0,row=3)
 output_entry_variable = tk.StringVar()
 output_entry = tk.Entry(main_window, textvariable=output_entry_variable).grid(column=1, row=3)
 output_entry_btn = tk.Button(text="...", height = 2, width=10, command = open_out_dir_chooser).grid(column=2, row=3)
 
-start_btn = tk.Button(main_window,text="Start", fg = "#000000", bg = "#00FF55", height = 2, width = 20, command = start_training).grid(column=0,row=4)
+image_width_label = tk.Label(main_window, text="Sample Image width", pady=3).grid(column=0, row=4)
+image_width_variable = tk.StringVar()
+image_width_entry = tk.Entry(main_window, textvariable=image_width_variable).grid(column=1, row=4)
+image_width_variable.set('24')
+
+image_height_label = tk.Label(main_window, text="Sample Image height", pady=3).grid(column=2, row=4)
+image_height_variable = tk.StringVar()
+image_height_entry = tk.Entry(main_window, textvariable=image_height_variable).grid(column=3, row=4)
+image_height_variable.set('24')
+
+sample_usage_percent_lable = tk.Label(main_window, text="Sample image usage percent", pady=3).grid(column=0,row=5)
+sample_usage_percent_variable = tk.StringVar()
+sample_usage_percent_entry = tk.Entry(main_window, textvariable=sample_usage_percent_variable).grid(column=1, row=5)
+sample_usage_percent_variable.set('100')
+
+start_btn = tk.Button(main_window,text="Start", fg = "#000000", bg = "#00FF55", height = 2, width = 20, command = start_training).grid(column=0,row=6)
 
 main_window.mainloop()
